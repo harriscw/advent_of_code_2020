787c74e0c69b17267f13f5cc4e71c80bf243e388
Harris <harric17@nala.roche.com>
2020-12-01 14:59:28 -0800

    'refactor'

--- day01/part1.py
+++ day01/part1.py
@@ -17,18 +17,11 @@ for line in reader:
 	for i in range(len(line)):
 		mylist.append(int(line[i]))
 
-#print(mylist)
-
-myiter=iter(range(len(mylist)))
-
-for i in myiter:
+for i in iter(range(len(mylist))):
 	mylist2 = np.add(mylist,mylist[i])
 	gotit=any(x ==2020 for x in mylist2)
 	if(gotit==True):
-		
-		#print(mylist[i])
 		theindex = np.where(mylist2 == 2020)[0][0]
-		#print(theindex)
 		print(str(mylist[i]) + str("+") + str(mylist[theindex]) + str("=") + str(mylist[i]+mylist[theindex]))
 		print(str(mylist[i]) + str("*") + str(mylist[theindex]) + str("=") + str(mylist[i]*mylist[theindex]))
 		
