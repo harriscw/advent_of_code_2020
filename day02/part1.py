7e5387b00b322015c42f58b39a70f051d9d00be7
Harris <harric17@nala.roche.com>
2020-12-02 10:16:49 -0800

    'd2p1'

--- day02/part1.py
+++ day02/part1.py
@@ -1,6 +1,12 @@
 import csv
-from pandas import DataFrame
+import pandas as pd
 
+# Read data
+
+mylist = [
+'1-3 a: abcde',
+'1-3 b: cdefg',
+'2-9 c: ccccccccc']
 
 file=open("input.csv", "rt")
 reader = csv.reader(file)
@@ -8,11 +14,21 @@ mylist = []
 for line in reader:
 	for i in range(len(line)):
 		mylist.append(line[i])
-		
-df_raw = DataFrame (mylist,columns=['raw'])
-print(df_raw)
 
-df_pw = df_raw['raw'].str.split(pat=": ",expand=True)
-print(df_pw)
-df_criteria = df_pw[0].str.split(pat=" ",expand=True)
-print(df_criteria)
+#wrangle into pandas df
+df_raw = pd.DataFrame(mylist,columns=['raw']) #get raw string
+df_pw = df_raw['raw'].str.split(pat=": ",expand=True) #get pw string
+df_letter = df_pw[0].str.split(pat=" ",expand=True)#get letter
+df_minmax = df_letter[0].str.split(pat="-",expand=True)#get min/max
+#concatenate together
+df = pd.concat([df_minmax[0],df_minmax[1],df_letter[1],df_pw[1],df_raw],axis=1)
+df.columns = ['min', 'max','letter','pw','raw']
+
+#get counts
+keepers=[]
+for i in iter(range(len(df.index))):
+	thecount=df['pw'][i].count(df['letter'][i])
+	keepers.append(int(df['min'][i])<=thecount<=int(df['max'][i]))
+
+print(keepers)
+print(len(df[keepers].index))
