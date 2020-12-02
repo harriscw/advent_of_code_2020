ecf0c7e11022ffc661866e20eadfebf408b83a1c
Harris <harric17@nala.roche.com>
2020-12-02 10:18:54 -0800

    d2p1 updt

--- day02/part1.py
+++ day02/part1.py
@@ -2,7 +2,6 @@ import csv
 import pandas as pd
 
 # Read data
-
 mylist = [
 '1-3 a: abcde',
 '1-3 b: cdefg',
@@ -20,15 +19,14 @@ df_raw = pd.DataFrame(mylist,columns=['raw']) #get raw string
 df_pw = df_raw['raw'].str.split(pat=": ",expand=True) #get pw string
 df_letter = df_pw[0].str.split(pat=" ",expand=True)#get letter
 df_minmax = df_letter[0].str.split(pat="-",expand=True)#get min/max
-#concatenate together
-df = pd.concat([df_minmax[0],df_minmax[1],df_letter[1],df_pw[1],df_raw],axis=1)
-df.columns = ['min', 'max','letter','pw','raw']
+
+df = pd.concat([df_minmax[0],df_minmax[1],df_letter[1],df_pw[1],df_raw],axis=1)#concatenate together
+df.columns = ['min', 'max','letter','pw','raw']#name columns
 
 #get counts
 keepers=[]
 for i in iter(range(len(df.index))):
-	thecount=df['pw'][i].count(df['letter'][i])
-	keepers.append(int(df['min'][i])<=thecount<=int(df['max'][i]))
+	thecount=df['pw'][i].count(df['letter'][i]) #get count
+	keepers.append(int(df['min'][i])<=thecount<=int(df['max'][i])) #does it meet criteria
 
-print(keepers)
-print(len(df[keepers].index))
+print(sum(keepers))
