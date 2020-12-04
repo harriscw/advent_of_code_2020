027e45fc18651e92d214cdd14637a204635d1f5c
Harris <harric17@nala.roche.com>
2020-12-04 05:36:23 -0800

    'd4p1'

---
+++ day04/part1.py
@@ -0,0 +1,28 @@
+import csv
+import re
+
+#Read data 
+file=open("input2.csv", "rt")
+reader = csv.reader(file)
+mylist = []
+for line in reader:
+	if line==[]:
+		line=["xxxx"]
+	for i in range(len(line)):
+		mylist.append(line[i])
+
+#make each passport a single string in a list
+wrangled=''.join(mylist).split("xxxx")
+
+#define regular expressions to match
+regexes= ['byr', 'iyr', 'eyr','hgt','hcl','ecl','pid','cid']
+combinedRegex = re.compile('|'.join('(?:{0})'.format(x) for x in regexes))
+
+# find matches
+finallist=[]
+for line in wrangled:
+	thematches=combinedRegex.findall(line)
+	if (len(thematches)==8)|(len(thematches)==7 and "cid" not in thematches):#either all or there or all but cid
+		finallist.append(True)
+
+print("Total Valid Passports: " + str(sum(finallist)))
