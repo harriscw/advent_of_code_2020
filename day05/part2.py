e505116423534d39d0655d894b7bd250b3f322b9
Harris <harric17@nala.roche.com>
2020-12-04 23:21:34 -0800

    'comments'

--- day05/part2.py
+++ day05/part2.py
@@ -1,5 +1,7 @@
 import csv
 
+#Read data 
+
 #mylist=[
 #"FBFBBFFRLR",
 #"BFFFBBFRRR",
@@ -7,7 +9,6 @@ import csv
 #"BBFFBBFRLL"
 #]
 
-#Read data 
 file=open("input.csv", "rt")
 reader = csv.reader(file)
 mylist = []
@@ -19,7 +20,7 @@ outlist=[]
 for line in mylist:
 	#print(line)
 	myrange=range(127)
-	for letter in line:
+	for letter in line[0:7]: #iterate over first 7 FB characters
 		if letter=="F":
 			myrange=myrange[0:int((len(myrange))/2)]
 		elif letter=="B":
@@ -28,14 +29,15 @@ for line in mylist:
 		#print(letter + " " + str(myrange) + " last:" + str(lastnum))
 		
 	myrange2=range(7)
-	for letter2 in line[7:10]:
+	for letter2 in line[7:10]: #iterate over last 3 RL characters
 		if letter2=="L":
 			myrange2=myrange2[0:int((len(myrange2))/2)]
 		elif letter2=="R":
 			myrange2=myrange2[int((len(myrange2)+1)/2):int((len(myrange2)))]
 		lastnum2=myrange2.start
 		#print(letter2 + " " + str(myrange2) + " last:" + str(lastnum2))
-	outlist.append(lastnum*8+lastnum2)
+		
+	outlist.append(lastnum*8+lastnum2)#do the required final math
+	
 print(sorted(outlist))
-
 print(set(outlist) ^ set(range(min(outlist),max(outlist))))
