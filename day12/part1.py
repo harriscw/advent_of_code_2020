c41172f086899b10cdaebd7019c309de037e55f5
Harris <harric17@nala.roche.com>
2020-12-13 16:00:01 -0800

    'd12p1'

--- day12/part1.py
+++ day12/part1.py
@@ -15,7 +15,8 @@ mypos = [0,0,"E"]
 for instruc in mylist:
 	print(mypos)
 	print(instruc)
-	
+
+#general instructions	
 	if instruc[0:1]=="N":
 		mypos[0]+=int(instruc[1:])
 	elif instruc[0:1]=="S":
@@ -24,6 +25,8 @@ for instruc in mylist:
 		mypos[1]+=int(instruc[1:])
 	elif instruc[0:1]=="W":
 		mypos[1]-=int(instruc[1:])
+		
+#instructions are dependent on which way the boat is facing
 	elif instruc[0:1]=="F":
 		if mypos[2]=="N":
 			mypos[0]+=int(instruc[1:])
@@ -33,20 +36,22 @@ for instruc in mylist:
 			mypos[1]+=int(instruc[1:])
 		elif mypos[2]=="W":
 			mypos[1]-=int(instruc[1:])
+			
+#instructions to change the direction of the boat
 	else:
-		steps=int(instruc[1:])/90
-		mydirs=["E","S","W","N"]
-		if instruc[0:1]=="L":
+		steps=int(instruc[1:])/90 #number of steps for iterator
+		mydirs=["E","S","W","N"] #set directions in order
+		if instruc[0:1]=="L": #reverse as needed
 			mydirs.reverse()
-		mydirs=cycle(mydirs) #ugh I accidentally indented this line and it took me forever to debug
+		mydirs=cycle(mydirs) #use a cyclical iteraotr.  ugh I accidentally indented this line and it took me forever to debug
 		seen=False
 		stepdone=0
 		for direction in mydirs:
-			if direction==mypos[2]:
+			if direction==mypos[2]: #start keeping track after the current direction is seen
 				seen=True
 			if seen==True:
 				stepdone+=1
-			if stepdone>steps:
+			if stepdone>steps:# stop after the number of steps is done
 				mypos[2]=direction
 				break
 
