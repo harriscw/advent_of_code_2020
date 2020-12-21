b88d12527710931042e7d7a92657ea6c18fbf8d9
Harris <harric17@nala.roche.com>
2020-12-21 13:22:36 -0800

    d18p2

--- day18/part2.py
+++ day18/part2.py
@@ -2,15 +2,15 @@ import sys
 import re
 
 #Read data 
-text_file = open("input0.txt", "r")
+text_file = open("input.txt", "r")
 lines = text_file.readlines()
 mylist=[]
 for i in range(len(lines)):
 	mylist.append(lines[i].strip("\n"))
-	
+
 #define some regular expressions
 innerparens = re.compile("\([^()]+\)") #find the inner most parentheses (have open and close with no other parens in between)
-startplus = re.compile("^([0-9]+ \+ [0-9]+)") #find additions
+anyplus = re.compile("([0-9]+ \+ [0-9]+)") #find additions
 starttimes = re.compile("^([0-9]+ \* [0-9]+)") #find multiplications
 
 #write a function to resolve a string with no parentheses from left to right
@@ -21,28 +21,23 @@ def resolvestring(string):
 	while keepgoing==True:	#keep going until the string is completely resolved
 		#print("Step",cnt)
 		#print("Original String",thestring)
-		print(startplus.findall(thestring))
-		if len(startplus.findall(thestring))>0: #if string starts with addition grep, resolve, and substitute
-			thematch=startplus.findall(thestring)[0] #grep
+		plusmatch=anyplus.findall(thestring)
+		#print("Match List:",plusmatch)
+		if len(plusmatch)>0:
+			firstmatch=plusmatch[0] #need to do this one at a time for only the first match
+			thematch=anyplus.findall(thestring)[0] #grep
 			resolved=str(eval(thematch)) #resolve
-			thestring=re.sub(r'^([0-9]+ \+ [0-9]+)',resolved,thestring) #substitute
-		elif len(starttimes.findall(thestring))>0: #if string starts with multiplication grep, resolve, and substitute
-			thematch=starttimes.findall(thestring)[0] #grep
-			resolved=str(eval(thematch)) #resolve
-			thestring=re.sub(r'^([0-9]+ \* [0-9]+)',resolved,thestring) #substitute
+			#print(thestring)
+			thestring=thestring.replace(firstmatch,resolved)
+			#print(thestring)
 		else:
-			sys.exit("Error couldn't resolve: " + string)
-			
-		keepgoing = "+" in thestring or "*" in thestring #stop the loop if theres no + or * in the string
-		#print(thematch)
-		#print(resolved)
-		#print(thestring)
-		#print(keepgoing)
+			keepgoing=False
 		cnt+=1
+	
+	thestring=str(eval(thestring))#once all the + are gone just evaluate the string
 	return(thestring)	
 
-print(resolvestring(mylist[0]))
-sys.exit("stop")
+
 outlist=[]
 for eachstring in mylist: #iterate through each equation in the problem set
 	#print(eachstring)
@@ -69,7 +64,6 @@ for eachstring in mylist: #iterate through each equation in the problem set
 		mycnt+=1
 				
 	#When no inner parentheses exist anymore just resolve the string
-	#print(resolvestring(thisstring))
 	outlist.append(int(resolvestring(thisstring)))
-		
+
 print("Solution:",sum(outlist))
