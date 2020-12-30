8eeab009087cc7eede47cd4df28db9c5518eadbb
Harris <harric17@nala.roche.com>
2020-12-29 20:00:53 -0800

    d21p2

--- day21/part2.py
+++ day21/part2.py
@@ -6,11 +6,11 @@ lines = text_file.readlines()
 mylist=[]
 mealdict=dict()
 cnt=0
-for i in range(len(lines)):
+for i in range(len(lines)): #create a dictionary of dictionaries. Key is line number, subentries are ingredients and allergens
 	theline=lines[i].strip("\n").replace("(","").replace(")","").split(" contains ")
-	ingredients=theline[0].split(" ")
-	allergens=theline[1].split(", ")
-	mealdict[cnt]={"ingredients":ingredients,"allergens":allergens}
+	ingredients=theline[0].split(" ")#get ingredients
+	allergens=theline[1].split(", ")#get allergens
+	mealdict[cnt]={"ingredients":ingredients,"allergens":allergens}#create a dictionary of dictionaries.
 	cnt+=1
 #print(mealdict)
 print(len(mealdict.keys()))
@@ -24,29 +24,18 @@ for i in range(len(mealdict.keys())):
 	
 print(list(set(allergenlist)))
 print(min(inglist))
+
 #iterate through and see if there's one ingredient that is in all dishes labelled with a certain allergen
-
 allergendict=dict()
-for theallergen in list(set(allergenlist)):
+for theallergen in list(set(allergenlist)):#iterate over each unique allergen
 	allergenlist=[]
-	for i in range(len(mealdict.keys())):
-		if theallergen in mealdict[i]['allergens']:
-			allergenlist.append(mealdict[i]['ingredients'])
-	allergendict[theallergen]=list(set.intersection(*map(set,allergenlist)))
+	for i in range(len(mealdict.keys())):#for each meal
+		if theallergen in mealdict[i]['allergens']: #if the allergen is in that meal
+			allergenlist.append(mealdict[i]['ingredients'])#add it to a list of list
+	allergendict[theallergen]=list(set.intersection(*map(set,allergenlist)))#get the intersection of each sublist
 	print(theallergen,"suspects:",set.intersection(*map(set,allergenlist)))
-#print(allergendict)
 
-#unknownallergens=list(allergendict.keys())
-#print(unknownallergens)
-#knowndict=dict()
-#while len(unknownallergens)>0:
-#	for key in unknownallergens:
-#		if len(allergendict[key])==1:
-#			knowndict[key]=allergendict[key][0]
-#			for i in unknownallergens:
-#			#unknownallergens.replace(key,"")
-#print(knowndict)
-	
+#it was faster to eyeball this than do it programmatically.  Maybe I am a bad person.
 #shellfish: clg
 #peanuts: lxjtns
 #sesame: vzzz
@@ -56,13 +45,15 @@ for theallergen in list(set(allergenlist)):
 #soy: cxfz
 #wheat: qdfpq
 
-#knownallergens =['mxmxvkd','sqjhc','fvjkl']
-knownallergens =['clg','lxjtns','vzzz','knprxg','ncjv','prxmdlz','cxfz','qdfpq']
+#it was faster to eyeball this than do it programmatically.  Maybe I am a bad person.
 
-cnt=0
-for i in mealdict.keys():
-	for j in mealdict[i]["ingredients"]:
-		if j not in knownallergens:
-			cnt+=1
-			
-print(cnt)
+#eggs: prxmdlz
+#fish: ncjv
+#nuts: knprxg
+#peanuts: lxjtns
+#sesame: vzzz
+#shellfish: clg
+#soy: cxfz
+#wheat: qdfpq
+
+#prxmdlz,ncjv,knprxg,lxjtns,vzzz,clg,cxfz,qdfpq
