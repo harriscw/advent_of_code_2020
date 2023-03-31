bc3b8fb1138b07b65cceb4eb79eb08fb90d7000c
harriscw <harriscw@ucla.edu>
2023-03-31 12:42:12 -0700

    conceptual p2"

---
+++ day01/r/another_solution.R
@@ -0,0 +1,35 @@
+#can we do this without loops
+
+#read data
+input = as.numeric(unlist(read.delim("input.txt", header = FALSE)))
+
+#lets use matrix addition
+m1 = matrix(rep(input,length(input)),nrow=length(input)) #matrix where each col is your input
+m2 = t(m1) #matrix where each row is your input
+mat = m1+m2
+
+#find the elements that = 2020
+res = which(mat==2020, arr.ind=TRUE)
+
+#multiply them together
+input[res[1,1]]*input[res[1,2]]
+
+
+
+# part 2
+
+#now you would need to do this as addition of 2 tensors (3d matrices)
+#you need to add each item of the input to each element of the matrix
+#you got in part 1 where each cell represents the addition of two
+# elements from the input.
+
+# so you would need to create tensor A which is mat from part 1 repeated
+# as many times as the input.  And tensor B which is the input vector repeated
+# multiple times into the same dimensions as tensor A.
+
+# so for the sample input of length 6, 'mat' from part 1 is 6x6.  
+# so tensor A is 6x6.  
+# and to tensor B is the length 6 input vector reshaped into a 6x6x6 tensor.
+
+#add tensor A to tensor B.  Find where the sum is 2020.  backtrack to individual
+# components.
