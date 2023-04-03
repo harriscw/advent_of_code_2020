581603740fe681e09094793139febac1ae271016
harriscw <harriscw@ucla.edu>
2023-04-03 09:55:31 -0700

    matrix solution

--- day01/r/another_solution.R
+++ day01/r/another_solution.R
@@ -3,14 +3,14 @@
 #read data
 input = as.numeric(unlist(read.delim("input.txt", header = FALSE)))
 
-input=c(
-  1721,
-  979,
-  366,
-  299,
-  675,
-  1456
-)
+# input=c(
+#   1721,
+#   979,
+#   366,
+#   299,
+#   675,
+#   1456
+# )
 
 #lets use matrix addition
 m1 = matrix(rep(input,length(input)),nrow=length(input)) #matrix where each col is your input
@@ -28,7 +28,7 @@ input[res[1,1]]*input[res[1,2]]
 # part 2
 
 #now you would need to do this as addition of 2 tensors (3d matrices)
-#you need to add each item of the input to each element of the matrix
+#you could add each item of the input to each element of the matrix
 #you got in part 1 where each cell represents the addition of two
 # elements from the input.
 
@@ -43,19 +43,21 @@ input[res[1,1]]*input[res[1,2]]
 #add tensor A to tensor B.  Find where the sum is 2020.  backtrack to individual
 # components.
 
-m1=array(rep(mat,length(input)),rep(length(input),3)) #make an nxnxn cube by stacking your solution to p1
-dim(m1)
+m1b=array(rep(mat,length(input)),rep(length(input),3)) #make an nxnxn cube by stacking your solution to p1
+dim(m1b)
 
 # You want to add each element of input to your previous answer
 # so create a 3D matrix where each slice is one number from input
 vec=sort(rep(input,length(input)^2))
-m2=array(vec,rep(length(input),3))
-m2
-dim(m2)
+m2b=array(vec,rep(length(input),3))
+m2b
+dim(m2b)
 
-mat2=m1+m2 #add them
-which(mat2==2020, arr.ind=TRUE) # get indices
+mat2=m1b+m2b #add them
+res=which(mat2==2020, arr.ind=TRUE) # get indices
+res
+# mat2[5,2,2]
+# m1b[,,2] #original part 1 solution
 
-mat2[5,2,2]
-m1[,,2] #original part 1 solution
-
+input[77]+input[62]+unique(vec)[1]
+input[77]*input[62]*unique(vec)[1]
